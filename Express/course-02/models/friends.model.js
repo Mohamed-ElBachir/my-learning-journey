992bdf843e6a25bee46ba1e68b9f49d45a84aaea
Mohamed-ElBachir <achour.med.bachir@gmail.com>
2023-11-15 22:38:09 +0100

    working with Model View Controller in Express

---
+++ Express/course-02/models/friends.model.js
@@ -0,0 +1,10 @@
+const friends = [{
+    id :0,
+    name : 'Issac Newton'
+},{
+    id :1,
+    name : 'Sir Issac Newtoon'
+}
+];
+
+module.exports = friends;
--- Express/course-02/server.js
+++ Express/course-02/server.js
@@ -1,38 +1,30 @@
 const express = require('express');
+const messagesController = require('./controllers/messages.controllers');
+const friendsController = require('./controllers/friends.controllers');
+
 
 const app = express();
 
 const PORT = 3000;
 
-const friends = [{
-    id :0,
-    name : 'Issac Newton'
-},{
-    id :1,
-    name : 'Sir Issac Newtoon'
-}
-]
 
-app.get('/friends', (req , res)=>{
-    res.json(friends)
-})
 
-app.get('/friends/:friendId', (req,res)=>{
-    const friendId = Number(req.params.friendId);
-    const firend = friends[friendId]; 
-    if(firend){
-        res.status(200).json(firend)
-    }
-    else{
-        res.status(404).json({
-            error : "friend does not exist "
-        });
-    }
-})
+app.use((req,res, next)=>{
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);    
+});
 
-app.get('/messages',(req,res)=>{
-    res.send('<ul><li>Hello Albert!</li></ul>')
-})
+app.use(express.json());
+
+app.post('/friends',friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend)
+
+
+app.get('/messages',messagesController.getMessages);
+app.post('/messages',messagesController.postMessage);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
