b410a10a865449d2935e8c01b4799e338a2d7a10
Mohamed-ElBachir <achour.med.bachir@gmail.com>
2023-12-07 19:50:01 +0100

    Using JOI library

--- Projects/book-store-api/app.js
+++ Projects/book-store-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi')
 // init App
 const app = express()
 
@@ -21,7 +22,7 @@ const books =[
         description:"About Rich dad and poor dad ",
         price:12,
         cover :"soft cover"
-    },
+    }
 ]
 
 
@@ -41,17 +42,35 @@ app.get("/api/books/:id",(req ,res)=>{
 });
 
 
-app.post('/api/books',(req , res)=>{
-    console.log(req.body)
-    
+app.post("/api/books" ,(req , res)=>{
+    /*
+    if (!req.body.title || req.body.title <3) {
+        return res.status(400).json("title is required and must be more than 3 characters")
+    }
+    we can use this code but for each key so we gonna write alot of code .
+    we have a better solution whoch is using a libreray called JOI 
+    */
+    const schema = Joi.object({
+        title :  Joi.string().trim().min(3).max(200).required(),
+        author : Joi.string().min(3).trim().max(200).required(),
+        description: Joi.string().trim().min(3).max(500).required(),
+        price: Joi.number().min(0).required(),
+        cover : Joi.string().trim().required()
+    });
+
+    const{ error }=schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({message : error.details[0].message})
+    }
+
     const book = {
         id: books.length + 1,
         title: req.body.title,
-        author: req.body.price,
+        author: req.body.author,
         description:req.body.description,
         price: req.body.price,
         cover: req.body.cover
-    }
+    };
 
     books.push(book);
     res.status(201).json(book) // 201 => created successfully
