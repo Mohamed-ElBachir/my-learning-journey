48d6de348ba5879941610484ed7f3f7e91658e4a
Mohamed-ElBachir <achour.med.bachir@gmail.com>
2023-12-14 15:47:35 +0100

    adding a book model

--- Projects/book-store-api/app.js
+++ Projects/book-store-api/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const BooksPath = require('./routes/books')
 const authorsPath = require('./routes/authors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
 
 // Connection To DB
 mongoose
-.connect("mongodb+srv://achourmedbachir:book-store-api@cluster0.fezjlmu.mongodb.net/")
+.connect(process.env.MONGO_URI)
 .then(()=>console.log("connection to MongoDB"))
 .catch((errror)=>console.log("Connection Failed to MongoDB!: ",errror))
 // init App
@@ -14,13 +16,12 @@ const app = express()
 // Apply Middlewares
 app.use(express.json())
 
-
 //Routes
 
 app.use("/api/books",BooksPath);
 app.use("/api/authors",authorsPath);
 
 // Running the server
-const PORT = 5000;
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
 
--- Projects/book-store-api/routes/books.js
+++ Projects/book-store-api/routes/books.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const Joi = require('joi')
-
+const asyncHandler = require('express-async-handler');
 const router = express.Router();
+const {validateCreateBook,validateUpdateBook,Book} = require('../models/Book')
 
-const books =[
-    {
-        id:1,
-        title:"Black Swan",
-        author:"Nassim Taleb",
-        description:"About Black Swan",
-        price:10,
-        cover :"soft cover"
-    },
-    {
-        id:2,
-        title:"Rich Dad Poor Dad",
-        author:"Robert Kiyosaki",
-        description:"About Rich dad and poor dad ",
-        price:12,
-        cover :"soft cover"
-    }
-]
+
 /* 
 *   @desc  Get All Books
 *   @route  /api/books
@@ -28,45 +11,55 @@ const books =[
 *   @access public
 
 */
-router.get("/",(req ,res)=>{
-    res.status(200).json(books);
-})
+router.get(
+    "/",
+    asyncHandler(async (req, res) => {
+      const books = await Book.find().populate("author",['_id','firstName','lastName']);
+      res.status(200).json(books);
+    })
+  );
 
-router.get("/:id",(req ,res)=>{
-    const book = books.find(b=> b.id === parseInt(req.params.id));
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findById(req.params.id).populate('author');
     if (book) {
-        res.status(200).json(book)
-    }else{
-        res.status(404).json({message:"Book not found"});
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: "book not found" });
     }
-});
+  })
+);
 
+/** 
+ *  @desc    Create new book
+ *  @route   /api/books
+ *  @method  POST
+ *  @access  private (only admin)
+ */
 
-router.post("/" ,(req , res)=>{
-    /*
-    if (!req.body.title || req.body.title <3) {
-        return res.status(400).json("title is required and must be more than 3 characters")
-    }
-    we can use this code but for each key so we gonna write alot of code .
-    we have a better solution whoch is using a libreray called JOI 
-    */
-    const {error} = validateCreateBook(req.body)
-    if (error) {
-        return res.status(400).json({message : error.details[0].message})
-    }
-
-    const book = {
-        id: books.length + 1,
+router.post(
+    "/",
+    asyncHandler(async (req, res) => {
+      const { error } = validateCreateBook(req.body);
+  
+      if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+      }
+  
+      const book = new Book({
         title: req.body.title,
         author: req.body.author,
-        description:req.body.description,
+        description: req.body.description,
         price: req.body.price,
-        cover: req.body.cover
-    };
-
-    books.push(book);
-    res.status(201).json(book) // 201 => created successfully
-})
+        cover: req.body.cover,
+      });
+  
+      const result = await book.save();
+      res.status(201).json(result);
+    })
+  );
 
 
 /* 
@@ -76,20 +69,32 @@ router.post("/" ,(req , res)=>{
 *   @access public
 
 */
-router.put("/:id",(req,res)=>{
-    const {error} = validateUpdateBook(req.body)
-    if (error) {
-        return res.status(400).json({message: error.details[0].message})
-    }
-
-    const book = books.find(b=> b.id === parseInt(req.params.id));
-    if (book) {
-        res.status(200).json({message : "book has been updated "})
-    }else{
-        res.status(404).json({message : "book not found"})
-    }
-
-})
+router.put(
+    "/:id",
+    asyncHandler(async (req, res) => {
+      const { error } = validateUpdateBook(req.body);
+  
+      if (error) {
+        return res.status(400).json({ message: error.details[0].message });
+      }
+  
+      const updatedBook = await Book.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {
+            title: req.body.title,
+            author: req.body.author,
+            description: req.body.description,
+            price: req.body.price,
+            cover: req.body.cover,
+          },
+        },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedBook);
+    })
+  );
 
 
 /* 
@@ -99,42 +104,20 @@ router.put("/:id",(req,res)=>{
 *   @access public
 
 */
-router.delete("/:id",(req,res)=>{
-    
-    const book = books.find(b=> b.id === parseInt(req.params.id));
-    if (book) {
-        res.status(200).json({message : "book has been deleted "})
-    }else{
-        res.status(404).json({message : "book not found"})
-    }
+router.delete(
+    "/:id",
+    asyncHandler(async (req, res) => {
+      const book = await Book.findById(req.params.id);
+      if (book) {
+        await Book.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "book has been deleted" });
+      } else {
+        res.status(404).json({ message: "book not found" });
+      }
+    })
+  );
 
-})
 
-//Validate create book
-function validateCreateBook(obj) {
-    const schema = Joi.object({
-        title :  Joi.string().trim().min(3).max(200).required(),
-        author : Joi.string().min(3).trim().max(200).required(),
-        description: Joi.string().trim().min(3).max(500).required(),
-        price: Joi.number().min(0).required(),
-        cover : Joi.string().trim().required()
-    });
-
-    return schema.validate(obj);
-}
-
-// Validate  Update Book
-function validateUpdateBook(obj) {
-    const schema = Joi.object({
-        title :  Joi.string().trim().min(3).max(200),
-        author : Joi.string().min(3).trim().max(200),
-        description: Joi.string().trim().min(3).max(500),
-        price: Joi.number().min(0),
-        cover : Joi.string().trim()
-    });
-
-    return schema.validate(obj);
-}
 
 
 module.exports = router;
