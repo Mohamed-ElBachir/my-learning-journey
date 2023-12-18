44b1477ff7473c700e95279bc46269f278261cce
Mohamed-ElBachir <achour.med.bachir@gmail.com>
2023-12-19 00:24:33 +0100

    authentification  done

--- Projects/menr-stack/backend/routes/userRoutes.js
+++ Projects/menr-stack/backend/routes/userRoutes.js
@@ -5,20 +5,33 @@ import {
     logoutCurrentUser,
     getAllusers,
     getCurrentUserProfile,
-    updateCurrentUserProfile
+    updateCurrentUserProfile,
+    deleteUserById,
+    getUserById,
+    updateUserByID
  }from "../controllers/userController.js";
 
 
      
 import {authorizeAdmin,authenticate} from '../middelwares/authMiddelware.js'
+import asynchandler from "../middelwares/asynchandler.js";
 
 const router = express.Router();
 
 router.route('/').post(createUser).get(authenticate,authorizeAdmin,getAllusers);
-router.post('/auth',loginUser)
-router.post('/logout',logoutCurrentUser)
-router.route('/profile').get(authenticate,getCurrentUserProfile).put(authenticate,updateCurrentUserProfile)
 
+router.post('/auth',loginUser)
+
+router.post('/logout',logoutCurrentUser)
+
+router.route('/profile')
+.get(authenticate,getCurrentUserProfile)
+.put(authenticate,updateCurrentUserProfile)
+
+// Admin routes
+router.route('/:id').delete(authenticate,authorizeAdmin, deleteUserById)
+.get(authenticate,authorizeAdmin,getUserById)
+.put(authenticate,authorizeAdmin,updateUserByID)
 
 
 export default router;
