8603788443378ece6245eed806b097c530584d84
Mohamed-ElBachir <achour.med.bachir@gmail.com>
2023-12-21 22:06:46 +0100

    working on Navigation

--- Projects/menr-stack/frontend/src/pages/Auth/Navigation.jsx
+++ Projects/menr-stack/frontend/src/pages/Auth/Navigation.jsx
@@ -1,8 +1,40 @@
-
+import { useState } from "react";
+import {
+  AiOutlineHome,
+  AiOutlineShopping,
+  AiOutlineLogin,
+  AiOutlineUserAdd,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import {FaHeart} from "react-icons/fa"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Navigation.css";
 function Navigation() {
-  return (
-    <div>Navigation</div>
-  )
+  const [dropownOpen , setDropownOpen] = useState(false);
+  const [showSidebar , setShowSidebar] = useState(false);
+
+  const toggelDropdown = ()=>{
+    setDropownOpen(!dropownOpen);
+  };
+
+  const toggelSidebar = ()=>{
+    setShowSidebar(!showSidebar)
+  };
+
+  const closeSidebar = ()=>{
+    setShowSidebar(false)
+  }
+
+
+  return (<div style={{zIndex : 999}} className={`${showSidebar ? "hidden" : "flex"} xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w-[4%] hover:w-[15%] h-[100vh] fixed`} id="navigation-container">
+    <div className="felx flex-col justify-center space-y-4">
+      <link to='/' className="flex items-center transition-transform transform hover:translate-x-2" />
+      <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+      <span className="hidden nav-item-name mt-[3rem]">HOME</span>
+      </div> 
+
+  </div>)
 }
 
-export default Navigation
+export default Navigation;
